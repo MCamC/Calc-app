9f1e88c7c2219c2376e41afd9dcfabeaa8af202e
Mai <mcamilacalbo@gmail.com>
2020-12-28 01:21:56 -0300

    First commit

---
+++ src/components/Result.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Result = (props) => {
+    debugger
+    console.log ("Renderización de Result", props)
+    return (
+        <div className="result">
+            <span>10</span>
+        </div>
+    )
+}
+
+export default Result 
