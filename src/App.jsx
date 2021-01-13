59cfff7d72086ff6915a0f5e8c08683998643688
Mai <mcamilacalbo@gmail.com>
2021-01-13 13:12:55 -0300

    Quinto

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
 import React from 'react'
 import Button from './components/Button'
+import Functions from './components/Functions'
+import MathOperations from './components/MathOperations'
 import Result from './components/Result'
 import './App.css'
 
@@ -27,21 +29,22 @@ const App = () => {
             <button>9</button>
             <button>0</button>
         </div>
-        <div className="functions">
-            <button>
-                clear
-            </button>
-            <button>
-                r
-            </button>
-        </div>
-        <div className="math-operations">
-            <button>+</button>
-            <button>-</button>
-            <button>*</button>
-            <button>/</button>
-            <button>=</button>
-        </div>
+
+        <Functions
+            onContentClear={() =>
+                console.log("Content Clear:")}
+            onDelete={() =>
+                console.log("onDelete:")}
+        />
+
+        <MathOperations 
+            onClickOperation={operation => 
+                console.log("Operation:", operation)
+            }
+            onClickEqual={equal =>
+                console.log("Equal:", equal)
+            }
+            />
     </main>)
 }
 
